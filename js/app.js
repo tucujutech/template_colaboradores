e780e534d16f8de3c7c05e14613a388ec6c2f3ab
Alexander Leão <alexandersilvadeleao23@gmail.com>
2019-07-18 14:34:49 -0300

    Primeiro commit para enviar o template

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.getElementById('modal').setAttribute('style', 'display: flex;');
+}
+
+function hideMenu() {
+  document.getElementById('modal').setAttribute('style', 'display: none;');
+}
